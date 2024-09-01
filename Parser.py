ab87ec3e45ebb02ed7017388ebfa78507bc75211
Sebastian Estrada <est21405@uvg.edu.gt>
2024-09-01 14:48:58 -0600

    Fuerza bruta

--- Parser.py
+++ Parser.py
@@ -67,8 +67,63 @@ def Parser(regex):
       stack.append(top1)
   return stack
 
+def fuerzaBruta(expresion):
+    variables = [1, 2, 3, 4]  
+
+    for i in range(2 ** len(variables)):
+        asignacion = {}
+        for j, var in enumerate(variables):
+            asignacion[var] = (i & (1 << j)) != 0
+
+        satisfacible = True
+        for clausula in expresion:
+            clausula_verdadera = False
+            for literal in clausula:
+                if literal > 0 and asignacion[literal]:
+                    clausula_verdadera = True 
+                    break
+                elif literal < 0 and not asignacion[-literal]:
+                    clausula_verdadera = True
+                    break
+            if not clausula_verdadera:
+                satisfacible = False
+                break
+
+        if satisfacible:
+            return True, asignacion
+    return False, None
+
+def parseToNumber(valor):
+  for i in range(len(valor)):
+    for j in range(len(valor[i])):
+      if valor[i][j] == 'p':
+        valor[i][j] = 1
+      elif valor[i][j] == 'q':
+        valor[i][j] = 2
+      elif valor[i][j] == 'r':
+        valor[i][j] = 3
+      elif valor[i][j] == 's':
+        valor[i][j] = 4
+  return valor
+
+def parseToString(valor):
+  return {
+    'p': valor.get(1), 
+    'q': valor.get(2), 
+    'r': valor.get(3), 
+    's': valor.get(4)
+  }
+
 
 regex = '(p ∨ q) ∧ (q ∨ s) ∧ (p ∨ s) ∧ (q ∨ s) '
 rt = infix_to_Postfix(regex)
 valor = Parser(rt)
 print(valor)
+
+result, asignacion = fuerzaBruta(parseToNumber(valor))
+
+if result:
+    print("La expresión es satisfacible")
+    print(parseToString(asignacion))
+else:
+    print("La expresión no es satisfacible")
